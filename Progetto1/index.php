0d82e53fd45f26840a0ed129d8a248e8d61e79e6
amm <amm@amm-VirtualBox>
2015-03-11 19:21:12 +0100

    vers1

--- Progetto1/index.php
+++ Progetto1/index.php
@@ -10,7 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <h1>ggg</h1>
+        <h1>gggg</h1>
         <?php
         // put your code here
         ?>
